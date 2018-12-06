da4f6852994bd83405f2b172e8b0e09c3d8d5dde
agx88 <peppe-a@live.it>
2018-12-06 17:45:53 +0100

    finished Day3

--- Day3/slice_it.js
+++ Day3/slice_it.js
@@ -1,6 +1,6 @@
 'use strict'
 const fs = require('fs');
-
+// PART 1
 const parse = row => {
     let leftop = row.split("@")[1].split(":")[0].split(",");
     let wh = row.split("@")[1].split(":")[1].split("x")
@@ -22,8 +22,28 @@ const getIndexes = coords => {
     return indexes;
 }
 
-const computeOverlaps = input => {
-    let sum = 0;
+// PART 1
+// const computeOverlapArea = input => {
+//     let sum = 0;
+//     let hmap = {};
+//     input.forEach(row => {
+//         let indexes = getIndexes(parse(row));
+//         indexes.forEach(pair => {
+//             if (hmap[pair]) {
+//                 if (hmap[pair] == 1) {
+//                     hmap[pair] = 'X';
+//                     sum++;
+//                 }
+//             }
+//             else hmap[pair] = 1;
+//         })
+//     })
+//     return sum;
+// }
+
+// PART 2
+const computeNonOverlapId = input => {
+    //Build
     let hmap = {};
     input.forEach(row => {
         let indexes = getIndexes(parse(row));
@@ -31,15 +51,32 @@ const computeOverlaps = input => {
             if (hmap[pair]) {
                 if (hmap[pair] == 1) {
                     hmap[pair] = 'X';
-                    sum++;
                 }
             }
             else hmap[pair] = 1;
         })
     })
-    return sum;
+
+    for (let row of input) {
+        let indexes = getIndexes(parse(row));
+        let i = 0;
+        for (i; i < indexes.length; i++) {
+            if (hmap[indexes[i]] == 'X') {
+                break;
+            }
+        }
+        if (i == indexes.length) {
+            return row.split('@')[0].split('#')[1]; //id of the row
+        }
+    }
 }
 
-fs.readFile( __dirname + `/input.txt`, (err, data) => {
-    console.log(computeOverlaps(data.toString().split('\n')));
+// console.log(computeNonOverlapId([
+//     "#1 @ 1,3: 4x4",
+//     "#2 @ 3,1: 4x4",
+//     "#3 @ 5,5: 2x2"
+// ]))
+
+fs.readFile(__dirname + `/input.txt`, (err, data) => {
+    console.log(computeNonOverlapId(data.toString().split('\n')));
 })
